7951acb09b24749897314c8deb50c74bacb44b86
Neeraj kumar <neerajkumar08697@gmail.com>
2024-06-11 14:27:13 +0530

    add country details page

--- countryDetails.js
+++ countryDetails.js
@@ -1,24 +1,14 @@
 const countryDetails = document.querySelector(".country-details");
-const countryName = document.querySelector(".country-name");
-const countryFlag = document.querySelector(".country-flag");
-const nativeName = document.querySelector(".native-name");
-const population = document.querySelector(".population");
-const region = document.querySelector(".region");
-const subRegion = document.querySelector(".sub-region");
-const capital = document.querySelector(".capital");
-const topLevelDomain = document.querySelector(".top-level-domain");
-const currencies = document.querySelector(".currencies");
-const languages = document.querySelector(".languages");
-const borderCountries = document.querySelector(".border-countries");
 const body = document.querySelector("body");
 const shimmerCountryDetails = document.querySelector(
   ".shimmer-country-details"
 );
 
+shimmerCountryDetails.classList.remove("hide");
+
 const nameParam = new URLSearchParams(location.search)
   .get("name")
   .toLowerCase();
-console.log(nameParam);
 const backBtn = document.querySelector(".back-button");
 backBtn.addEventListener("click", () => {
   history.back();
@@ -27,53 +17,106 @@ backBtn.addEventListener("click", () => {
 fetch(`https://restcountries.com/v3.1/name/${nameParam}?fullText=true`)
   .then((response) => response.json())
   .then(([country]) => {
+    shimmerCountryDetails.classList.add("hide");
+    const countryData = {};
     if (country) {
-      console.log(country);
-      countryFlag.src = country.flags.svg;
-      countryName.innerHTML = country.name.common;
-      population.innerHTML = country.population.toLocaleString("en-IN");
-      region.innerHTML = country.region;
-      topLevelDomain.innerHTML = country.tld;
+      countryData.countryFlag = country.flags.svg;
+      countryData.countryName = country.name.common;
+      countryData.population = country.population.toLocaleString("en-IN");
+      countryData.region = country.region;
+      countryData.topLevelDomain = country.tld;
 
       if (country.subregion) {
-        subRegion.innerHTML = country.subregion;
+        countryData.subRegion = country.subregion;
       }
 
       if (country.capital) {
-        capital.innerHTML = country.capital?.[0];
+        countryData.capital = country.capital?.[0];
       } else {
-        capital.innerHTML = country.capital;
+        countryData.capital = country.capital;
       }
       if (country.currencies) {
-        currencies.innerHTML = Object.values(country.currencies).map(
+        countryData.currencies = Object.values(country.currencies).map(
           (currency) => currency.name
         );
       }
       if (country.name.nativeName) {
-        nativeName.innerHTML = Object.values(country.name.nativeName)[0].common;
+        countryData.nativeName = Object.values(
+          country.name.nativeName
+        )[0].common;
       } else {
-        nativeName.innerHTML = country.name.common;
+        countryData.nativeName = country.name.common;
       }
       if (country.languages) {
-        languages.innerHTML = Object.values(country.languages).join(", ");
+        countryData.languages = Object.values(country.languages).join(", ");
       }
-      if (country.borders) {
-        country.borders.forEach((border) =>
-          fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-            .then((res) => res.json())
-            .then(([bordersCountry]) => {
-              const borderCountryTag = document.createElement("a");
-              if (bordersCountry.name.common) {
-                borderCountryTag.innerText = bordersCountry.name.common;
-                borderCountryTag.href = `/countryDetails.html?name=${bordersCountry.name.common}`;
-                borderCountries.append(borderCountryTag);
-              }
-            })
-        );
-      }
+
+      // if (country.borders) {
+      //   country.borders.forEach((border) =>
+      //     fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+      //       .then((res) => res.json())
+      //       .then(([bordersCountry]) => {
+      //         let countryBordersList = [];
+      //         if (bordersCountry.name.common) {
+      //           countryBordersList.push(bordersCountry);
+      //           // countryBordersList.push(`<a href="/countryDetails.html?name=${bordersCountry.name.common}">${bordersCountry.name.common}</a>`);
+      //           countryData.rrrrr = countryBordersList;
+      //           console.log(countryBordersList);
+      //         }
+      //       })
+      //   );
+      // }
+
+      displayCountryDetails(countryData);
     }
   })
   .catch((error) => console.log(error));
+
+function displayCountryDetails(countryData) {
+  countryDetails.innerHTML = `
+   <img src=${countryData.countryFlag} alt="" class="country-flag">
+             <div class="details-text-container">
+                <h1 class="country-name">${countryData.countryName}</h1>
+                <div class="details-text">
+                  <p>
+                    <b>Native Name:</b>
+                    <span class="native-name">${countryData.nativeName}</span>
+                  </p>
+                  <p>
+                    <b>Population:</b>
+                    <span class="population">${countryData.population}</span>
+                  </p>
+                  <p>
+                    <b>Region: </b>
+                    <span class="region">${countryData.region}</span>
+                  </p>
+                  <p>
+                    <b>Sub Region:</b>
+                    <span class="sub-region">${countryData.subRegion}</span>
+                  </p>
+                  <p>
+                     <b>Capital: </b>
+                    <span class="capital">${countryData.capital}</span>
+                  </p>
+                  <p>
+                    <b>Top Level Domain:</b>
+                    <span class="top-level-domain">${countryData.topLevelDomain}</span>
+                  </p>
+                  <p>
+                    <b>Currencies:</b>
+                    <span class="currencies">${countryData.currencies}</span>
+                  </p>
+                  <p>
+                    <b>Languages:</b>
+                    <span class="languages">${countryData.languages}</span>
+                  </p>
+                </div>
+                <div class="border-countries">             
+ 
+                  &nbsp;
+                </div>
+              </div>`;
+}
 
 const themeChangerBtn = document.querySelector(".theme-changer");
 
@@ -94,3 +137,9 @@ themeChangerBtn.addEventListener("click", () => {
     themeChangerBtn.innerHTML = `<i class="fa-regular fa-moon"></i>Dark Mode`;
   }
 });
+
+/*
+  <p class="borders">Border Countries:
+   </p>
+
+*/
